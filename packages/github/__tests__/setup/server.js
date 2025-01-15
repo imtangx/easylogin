5807cfa8858517e7bfdb28e0e8401e5a8aa1cef9
imtangx <939174106@qq.com>
2025-01-15 16:23:02 +0800

    chore(server): 将测试服务器迁至测试目录

--- packages/github/__tests__/setup/server.js
+++ packages/github/__tests__/setup/server.js
@@ -1,18 +1,21 @@
 import express from 'express';
-import { GithubLogin } from '@esaylogin/github';
-import { GITHUB_SCOPES } from './constant';
+import { GithubLogin } from '../../src/index';
+import { GITHUB_SCOPES } from '../../src/constant';
+import { config } from 'dotenv';
+
+config();
 
 const app = express();
 
 const githubLogin = new GithubLogin({
-  clientId: 'Ov23liZdAsLqruoCIh3q',
-  clientSecret: '780335aadab83af543ee312fa32730bc20a2e00e',
-  callbackUrl: 'http://localhost:8080/auth/github/callback',
+  clientId: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackUrl: process.env.GITHUB_CALLBACK_URL,
   scope: [...GITHUB_SCOPES.MINIMAL],
 });
 
 app.get('/auth/github', (req, res) => {
-  const url = githubLogin.getAuthUrl(); 
+  const url = githubLogin.getAuthUrl();
   res.redirect(url);
 });
 
@@ -27,7 +30,7 @@ app.get('/auth/github/callback', async (req, res) => {
   try {
     const response = await githubLogin.handleCallback(code);
     res.json(response);
-  } catch (error: any) {
+  } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
