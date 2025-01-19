b8740a6785212c547a57a987d900a2e11029e7c2
imtangx <939174106@qq.com>
2025-01-19 20:21:35 +0800

    chore(github): 优化配置

--- packages/github/vite.config.ts
+++ packages/github/vite.config.ts
@@ -3,15 +3,15 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     watch: {
-      include: 'src/**'  // 监听src目录下的所有文件
+      include: 'src/**', // 监听src目录下的所有文件
     },
     lib: {
       entry: 'src/index.ts',
       formats: ['es', 'cjs'],
-      fileName: (format) => `index.${format}.js`
+      fileName: format => `index.${format === 'es' ? 'mjs' : 'cjs'}`,
     },
     rollupOptions: {
-      external: ['express', 'axios', 'dotenv']
-    }
-  }
+      external: ['express', 'axios'],
+    },
+  },
 });
